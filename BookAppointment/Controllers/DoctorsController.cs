6096d40ffde47feb72894940340937a42c4ce9de
osama <ososah2001@gmail.com>
2025-02-08 15:44:44 +0200

    Add project files.

---
+++ BookAppointment/Controllers/DoctorsController.cs
@@ -0,0 +1,26 @@
+﻿using BookAppointment.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookAppointment.Controllers
+{
+    
+    public class DoctorsController : Controller
+    {
+        ApplicationDbContext dbContext = new ApplicationDbContext();
+
+       
+        public IActionResult BookAppointment()
+        {
+
+            var doctors = dbContext.Doctors;
+            return View(doctors);
+        }
+        public IActionResult CompleteAppointment(String doctorName)
+        {
+            var doctor = dbContext.Doctors.FirstOrDefault(d => d.Name == doctorName);
+
+            return View(doctor);
+        }
+      
+    }
+}
---
+++ BookAppointment/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using BookAppointment.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookAppointment.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Doctor> Doctors { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer("Data Source=.;Initial Catalog=taskmvc;Integrated Security=True;TrustServerCertificate=True");
+        }
+    }
+}
